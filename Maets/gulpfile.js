6f29ca2787a37dd6eb9e459334777701637e4dc4
krolebord <krolebord@gmail.com>
2022-04-16 22:25:36 +0300

    Add select2

--- Maets/gulpfile.js
+++ Maets/gulpfile.js
@@ -1,8 +1,13 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify-es').default;
 
 const vendorStyles = [
-    "node_modules/bootstrap-datepicker/dist/css/bootstrap-datepicker.css"
+    "node_modules/bootstrap-datepicker/dist/css/bootstrap-datepicker.css",
+    "node_modules/select2/dist/css/select2.css",
+    "wwwroot/css/bootstrap.css",
+    "wwwroot/css/select2-bootstrap-5-theme.css"
 ];
 const vendorScripts = [
     "node_modules/jquery/dist/jquery.js",
@@ -10,18 +15,21 @@ const vendorScripts = [
     "node_modules/jquery-validation-unobtrusive/dist/jquery.validate.unobtrusive.js",
     "node_modules/popper.js/dist/umd/index.js",
     "node_modules/bootstrap/dist/js/bootstrap.js",
-    "node_modules/bootstrap-datepicker/dist/js/bootstrap-datepicker.js"
+    "node_modules/bootstrap-datepicker/dist/js/bootstrap-datepicker.js",
+    "node_modules/select2/dist/js/select2.full.js"
 ];
 
 gulp.task('build-vendor-css', () => {
     return gulp.src(vendorStyles)
         .pipe(concat('vendor.min.css'))
+        .pipe(cleanCSS())
         .pipe(gulp.dest('wwwroot/css'));
 });
 
 gulp.task('build-vendor-js', () => {
     return gulp.src(vendorScripts)
         .pipe(concat('vendor.min.js'))
+        .pipe(uglify())
         .pipe(gulp.dest('wwwroot/js'));
 });
 
--- Maets/wwwroot/js/site.js
+++ Maets/wwwroot/js/site.js
@@ -1,3 +1,7 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 $('input[data-provide="datepicker"]').datepicker();
+
+$('select.search-select').select2({
+    theme: "bootstrap-5"
+});
